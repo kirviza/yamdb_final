9ca36449d987d6cc81b7a6651aa61207dc331e8d
Кирилл Вязовов <kirviza92@yandex.ru>
2022-07-31 22:17:23 +0300

    adf

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,9 +2,8 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .views import CategoryViewSet, GenreViewSet, TitleViewSet
-from .views import (ReviewViewSet, CommentViewSet,
-                    UserViewSet, get_jwt_token,
-                    register)
+from .views import CommentViewSet, ReviewViewSet, UserViewSet
+from .views import get_jwt_token, register
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
