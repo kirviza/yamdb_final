6ba2407c0d48567d6b30dcf0a559f50c2999ad5e
Кирилл Вязовов <kirviza92@yandex.ru>
2022-08-05 00:12:25 +0300

    сделал всё с нуля

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.201.161.37', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
